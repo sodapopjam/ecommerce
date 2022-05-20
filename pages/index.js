97e72baef3c251e5b94cbcc03ffa1ae4fdee0835
sodapopjam <sodapopjam@gmail.com>
2022-05-19 18:00:32 -0700

    Adding props from Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import { client } from '../lib/client'
 import { Product, FooterBanner, HeroBanner } from '../components'
 
-const Home = () => {
+const Home = ({ productData, bannerData }) => {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={ bannerData.length && bannerData[0] } />
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -13,12 +14,24 @@ const Home = () => {
       </div>
 
       <div className="products-container">
-        {['Poduct 1', 'Produc2'].map((product) => product)}
+        { productData?.map((product) => product.name) }
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]'
+  const productData = await client.fetch(productQuery)
+
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { productData, bannerData }
+  }
 }
 
 export default Home
