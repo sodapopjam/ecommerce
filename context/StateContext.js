9cc5774cd893c388f90f28b59ba1665d66fa1e11
sodapopjam <sodapopjam@gmail.com>
2022-05-24 01:06:27 -0700

    Adds stripe integration

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
 	const [totalQuantities, setTotalQuantities] = useState(0)
 	const [qty, setQty] = useState(1)
 
+	let foundProduct
+	let index
+
 	const onAdd = (product, quantity) => {
 		const checkProductInCart = cartItems.find((item) => item._id === product._id)
 		
@@ -35,6 +38,34 @@ export const StateContext = ({ children }) => {
 		toast.success(`${qty} ${product.name} added to the cart.`)
 	}
 
+	const onRemove = (product) => {
+		foundProduct = cartItems.find((item) => item._id === product._id)
+		const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+		setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+		setTotalQuantities(prevTotalQuantities => prevTotalQuantities - foundProduct.quantity)
+		setCartItems(newCartItems)
+		
+	}
+	const toggleCartItemQuantity = (id, value) => {
+		foundProduct = cartItems.find((item) => item._id === id)
+		index = cartItems.findIndex((product) => product._id === id)
+		const newCartItems = cartItems.filter((item) => item._id !== id)
+
+		if(value === 'inc') {
+			setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+			setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+			setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+		} else if(value === 'dec') {
+			if(foundProduct.quantity > 1)
+			{
+				setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
+				setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+				setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+			}
+		}
+	}
+
 	const incQty = () => {
 		setQty((prevQty) => prevQty + 1)
 	}
@@ -47,7 +78,20 @@ export const StateContext = ({ children }) => {
 
 	return (
 		<Context.Provider value={{
-			showCart, setShowCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd
+			showCart, 
+			setShowCart, 
+			cartItems, 
+			totalPrice, 
+			totalQuantities, 
+			qty, 
+			incQty, 
+			decQty, 
+			onAdd, 
+			toggleCartItemQuantity,
+			onRemove,
+			setCartItems,
+			setTotalPrice,
+			setTotalQuantities
 		}}>
 		{children}
 		</Context.Provider>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -51,7 +51,7 @@ const ProductDetails = ({ product, products }) => {
             <span className="minus" onClick={decQty}>
               <AiOutlineMinus />
             </span>
-            <span className="num" onClick="">
+            <span className="num">
               {qty}
             </span>
             <span className="plus" onClick={incQty}>
