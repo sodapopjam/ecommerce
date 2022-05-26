0fd9f32ccae535567d07fc4c9e1883e721b43bb7
sodapopjam <sodapopjam@gmail.com>
2022-05-25 18:20:43 -0700

    Updates to Sanity

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
 	<div className="navbar-container">
     <p className="logo">
-      <Link href="/">Cool Headphones</Link>
+      <Link href="/">Shopily</Link>
     </p>
     <button type="button" className="cart-icon" onClick={() => setShowCart(true) }>
       <AiOutlineShopping />
